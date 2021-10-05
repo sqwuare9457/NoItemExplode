6cb77d6b8918c8c4e42b2ce4fc4d499806623eca
KirkFox <kirkTWM.MC@gmail.com>
2021-10-04 18:41:40 -0700

    Implemented item filter and improved config.yml

--- src/main/java/me/kirkfox/noitemexplode/ConfigHandler.java
+++ src/main/java/me/kirkfox/noitemexplode/ConfigHandler.java
@@ -1,33 +1,48 @@
 package me.kirkfox.noitemexplode;
 
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ConfigHandler {
 
+    private static boolean whitelist = false;
+
+    private static final Set<Material> ITEM_FILTER = new HashSet<>();
     private static final Set<EntityType> PROTECTED_ENTITIES = new HashSet<>();
 
-    private static final String[] NAMES = {"creeper", "fireball", "tnt", "wither"};
-    private static final EntityType[][] ENTITIES = {{EntityType.CREEPER}, {EntityType.FIREBALL, EntityType.SMALL_FIREBALL},
-            {EntityType.PRIMED_TNT}, {EntityType.WITHER, EntityType.WITHER_SKULL}};
+    private static final String[][] ENTITIES = {{"creeper"}, {"fireball", "small_fireball"}, {"tnt", "primed_tnt"},
+            {"wither", "wither_skull"}};
 
     public static void loadConfig(JavaPlugin plugin) {
         plugin.saveDefaultConfig();
         FileConfiguration config = plugin.getConfig();
-        for(int i = 0; i < NAMES.length; i++) {
-            if(config.getBoolean("protect-from-" + NAMES[i] + "-explosions")) {
-                PROTECTED_ENTITIES.addAll(Arrays.asList(ENTITIES[i]));
+        whitelist = config.getBoolean("use-filter-as-whitelist");
+        List<String> filter = config.getStringList("filter");
+        for(String i : filter) {
+            Material m = Material.getMaterial(i.toUpperCase());
+            if (m != null) {
+                ITEM_FILTER.add(m);
+            }
+        }
+        ConfigurationSection pe = config.getConfigurationSection("protected-explosions");
+        for(String[] e : ENTITIES) {
+            if (pe == null || pe.getBoolean(e[0])) {
+                for(String et : e) {
+                    if(!et.equals("tnt")) PROTECTED_ENTITIES.add(EntityType.valueOf(et.toUpperCase()));
+                }
             }
         }
     }
 
-    public static boolean isProtectedEntity(EntityType e) {
-        return PROTECTED_ENTITIES.contains(e);
+    public static boolean shouldProtect(Material m, EntityType e) {
+        return PROTECTED_ENTITIES.contains(e) && (ITEM_FILTER.contains(m) == whitelist);
     }
 
 }
--- src/main/java/me/kirkfox/noitemexplode/listener/EntityDamageByEntityListener.java
+++ src/main/java/me/kirkfox/noitemexplode/listener/EntityDamageByEntityListener.java
@@ -2,7 +2,7 @@ package me.kirkfox.noitemexplode.listener;
 
 import me.kirkfox.noitemexplode.ConfigHandler;
 import me.kirkfox.noitemexplode.WorldStorage;
-import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -12,10 +12,12 @@ public class EntityDamageByEntityListener implements Listener {
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
-        if(e.getEntityType() == EntityType.DROPPED_ITEM && WorldStorage.isProtectedChunk(e.getEntity().getLocation().getChunk()) &&
-                e.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION &&
-                ConfigHandler.isProtectedEntity(e.getDamager().getType())) {
-            e.setCancelled(true);
+        if(e.getEntity() instanceof Item && WorldStorage.isProtectedChunk(e.getEntity().getLocation().getChunk()) &&
+                e.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) {
+            Item i = (Item) e.getEntity();
+            if(ConfigHandler.shouldProtect(i.getItemStack().getType(), e.getDamager().getType())) {
+                e.setCancelled(true);
+            }
         }
     }
 
