2c0f0a1fdfd8826f09e669e7b4f244b57334ca03
KirkFox <kirkTWM.MC@gmail.com>
2021-10-05 17:42:27 -0700

    Added simple update checker

--- src/main/java/me/kirkfox/noitemexplode/NoItemExplode.java
+++ src/main/java/me/kirkfox/noitemexplode/NoItemExplode.java
@@ -5,12 +5,16 @@ import me.kirkfox.noitemexplode.command.NIETabCompleter;
 import me.kirkfox.noitemexplode.listener.EntityDamageByEntityListener;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.SimplePie;
+import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Objects;
+import java.util.Scanner;
 
 public final class NoItemExplode extends JavaPlugin {
 
@@ -40,6 +44,8 @@ public final class NoItemExplode extends JavaPlugin {
         Metrics metrics = new Metrics(this, BSTATS_ID);
         metrics.addCustomChart(new SimplePie("worldsProtected", RegionStorage::getWorldsProtected));
 
+        checkForUpdates();
+
     }
 
     @Override
@@ -50,6 +56,23 @@ public final class NoItemExplode extends JavaPlugin {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void checkForUpdates() {
+        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+            try(InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=96571").openStream();
+                Scanner scanner = new Scanner(inputStream)) {
+                if(scanner.hasNext()) {
+                    String version = scanner.next();
+                    if (!this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                        this.getLogger().info("A new version of NoItemExplode is available. " +
+                                "Go to https://www.spigotmc.org/resources/noitemexplode.96571/ for NoItemExplode v" + version);
+                    }
+                }
+            } catch (IOException e) {
+                this.getLogger().info("Cannot look for updates: " + e.getMessage());
+            }
+        });
     }
 
     public static JavaPlugin getPlugin() {
